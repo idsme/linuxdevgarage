3aac568807d0b2e26a1c9635c0a991d4883ac999
Ids <idsmesales@gmail.com>
2023-07-23 01:57:37 +0200

    Update dev_garage_install_ide_vscode.sh

--- dev_garage_install_ide_vscode.sh
+++ dev_garage_install_ide_vscode.sh
@@ -1,12 +1,14 @@
 #/bin/bash
-echo "install VsCode" 
-sudo apt-get update
-# install vscode
-sudo apt-get -y install gconf-service gconf-service-backend gconf2-common libgconf-2-4
-curl -o vscode.deb -L http://go.microsoft.com/fwlink/?LinkID=760868
-sudo dpkg -i vscode.deb 
-sudo apt-get install -f
-sudo apt-get -y install code
-rm vscode.deb
+sudo snap install --classic code
+
+# Option 2
+#echo "install VsCode" 
+#sudo apt-get update
+#sudo apt-get -y install gconf-service gconf-service-backend gconf2-common libgconf-2-4
+#curl -o vscode.deb -L http://go.microsoft.com/fwlink/?LinkID=760868
+#sudo dpkg -i vscode.deb 
+#sudo apt-get install -f
+#sudo apt-get -y install code
+#rm vscode.deb
 echo "Finished VsCode Install"
 
