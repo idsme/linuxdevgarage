79841fe474d505126882a6dfc06c5ebe7bf53276
Ids <idsmesales@gmail.com>
2023-07-23 01:13:04 +0200

    Update dev_garage_install_frontend_tools.sh
    removed old content.. not needed anymore.

--- dev_garage_install_frontend_tools.sh
+++ dev_garage_install_frontend_tools.sh
@@ -1,40 +1,7 @@
 #/bin/bash
 echo "Install frontend tools" 
-#install nvm
-#install some global packages.
-npm install prettier --save-dev
-#make sure you have installed webstorm 2018.1 or higher.
-#also install https://plugins.jetbrains.com/plugin/10456-prettier
-#>Settings>Language & Frameworks > Javascript > ECMAScript .1
-#also install https://plugins.jetbrains.com/plugin/7294-editorconfig
-#ForceShortcuts plugin
 
-#Check>Language & Frameworks > Node.js and NPM 
-#Check>Node interpreter> points to correct node version
-#Check>Package manager> yarn points to correct yarn version
+#TODO install nvm
 
-#Configure this so you don't have to press Alt + Ctrl + Shift + P
-#https://prettier.io/docs/en/webstorm.html > Running Prettier on save using File Watcher
-
-#static type checker that brings type annotations to JavaScript
-#https://www.jetbrains.com/help/idea/using-the-flow-type-checker.html
-#npm install --global flow-bin
-
-
-npm install -g eslint
-#installes make or will fail during eslint --init
-sudo apt-get install build-essential
-eslint --init
-
-npm install --save-dev eslint-config-prettier
-
-https://github.com/airbnb/javascript
-
-#TODO set quotes to be generated in webstorm conform linting settings.
-#TODO webstorm want to go single quotes but prettier has formatted double quotes.
-#TODO check if AirBnB works.
-
-https://www.themarketingtechnologist.co/eslint-with-airbnb-javascript-style-guide-in-webstorm/
-
-echo "Finished frontend toolsl"
+echo "Finished frontend tools"
 
